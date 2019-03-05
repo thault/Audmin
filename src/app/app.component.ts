82f66b74c6ca7a71e2c3c64b356a6ef0dd9ecdbc
Tyler Ault <thault@gmail.com>
2019-03-04 19:14:33 -0500

    Adding a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule]})
 
 @Component({
   selector: 'app-root',
